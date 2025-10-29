47eb9fbcd72a655d24f876efb4efa27db7b45888
Gabriel <gab.dallacorte@gmail.com>
2025-10-29 22:59:59 +0100

    Add: size in the command line

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 """Create and solve a queen game."""
 
+import argparse
+
 from objects import Game
 
 
+def parse_arguments() -> argparse.Namespace:
+    """Parse the argumentsof the command line."""
+    parser = argparse.ArgumentParser(description="Create and solve a queen game.")
+    parser.add_argument("size", type=int, help='Size of the queen board.')
+    return parser.parse_args()
+
 def main() -> None:
     """Execute the code."""
-    test = Game(size=13)
+    args = parse_arguments()
+    size = args.size
+    test = Game(size=size)
     test.create_colored_rd_board()
     test.show()
     test.solve_board()
